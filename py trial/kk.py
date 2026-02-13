56a1d6bf6694a89314d02ce21e6a67a1ab6bebcc
shweta d <shwetadudhe44@gmail.com>
2026-02-13 18:05:50 +0530

    second commit

--- py trial/kk.py
+++ py trial/kk.py
@@ -5,3 +5,10 @@ year = 2026 - age
 
 print("Hello", name)
 print("You were born around", year)
+
+
+print("Next year you will be", age + 1, "years old")
+print("Have a nice day!")
+
+fav = input("What is your favorite color? ")
+print("Wow!", fav, "is a beautiful color!")
