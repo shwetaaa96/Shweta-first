1954e518d275919593516775454a68f058850207
shweta d <shwetadudhe44@gmail.com>
2026-02-13 19:20:08 +0530

    age

--- py trial/kk.py
+++ py trial/kk.py
@@ -12,3 +12,23 @@ print("Have a nice day!")
 
 fav = input("What is your favorite color? ")
 print("Wow!", fav, "is a beautiful color!")
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+year = 2026 - age
+
+print("Hello", name)
+print("You were born around", year)
+
+print("Next year you will be", age + 1, "years old")
+print("Have a nice day!")
+
+fav = input("What is your favorite color? ")
+print("Wow!", fav, "is a beautiful color!")
+
+hobby = input("What is your hobby? ")
+print("Nice! I hope you enjoy", hobby, "a lot.")
+
+country = input("Which country are you from? ")
+print("That's great! One day you will do something amazing in", country)
